43c50800ef6fab52da6700a04851ff79b068b363
javedalamthabrealam <51222868+javedalamthabrealam@users.noreply.github.com>
2019-07-15 15:38:35 +0530

    Update king.py

--- king.py
+++ king.py
@@ -3,17 +3,6 @@ l=list(map(int,input().split()))
 m=0
 
 for i in range(0,a):
-    iotethioetetetetetetetetetetetetetetetetetetetetetetetetetetetetetetetetetetet
-    \ghergregregerg
-    rgre
-    gerg
-    er
-    gr
-    ger
-    gre
-    ger
-    g
-    erg
 
     for j in range(0,i):
         if l[j]<l[i]:
