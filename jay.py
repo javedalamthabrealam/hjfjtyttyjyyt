29d002087f0fb5d78e447be18e8a9c720f729e76
javedalamthabrealam <51222868+javedalamthabrealam@users.noreply.github.com>
2019-07-15 15:30:36 +0530

    Create jay.py

---
+++ jay.py
@@ -0,0 +1,12 @@
+a,b = map(str,input().split())
+m = 0
+if len(a)>len(b):
+	a,b=b,a
+x = 0
+while x<len(a):
+	  m += (ord(b[x])-ord(a[x]))
+	  x += 1
+for x in range(x,len(b)):
+	  m += ord(b[x])-ord('a')+1
+print(m)
+print(d0)
